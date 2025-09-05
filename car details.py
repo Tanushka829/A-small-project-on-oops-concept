a44cd9537f3c52f2ea3e64280e23d8e8e88346a8
Tanushka829 <successisontheway321@gmail.com>
2025-09-05 19:18:51 +1300

    Update car details.py

--- car details.py
+++ car details.py
@@ -9,9 +9,10 @@ class car():
         price = float(input("enter price of car in lakh:"))
         print("car details are:")
         print("brand:",brand," and price(in lakh):",price)
-# we cant use name and age after 2nd def they both have different area
+# we cant use name and age after 2nd def they both lie in different sections.
 
 car1=car()
 car1.car_details()
 # perfect
+
 
